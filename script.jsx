cda77608dc668d1812c0c9b6adef37e7349d3c63
C.B <aschera@gmail.com>
2017-05-14 22:43:40 +0200

    Add files via upload

---
+++ script.jsx
@@ -0,0 +1,19 @@
+class Place extends React.Component {
+
+    render() {
+        return <span>hello</span>
+    }
+}
+
+
+
+
+/* ------------------------------------------------------------- */
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-. REACT DOM -.-.-.-.-.-.-.-.-.-.-. */
+/* ------------------------------------------------------------- */
+	ReactDOM.render(
+		<Place />,
+		document.getElementById('app')
+	);
+
+
